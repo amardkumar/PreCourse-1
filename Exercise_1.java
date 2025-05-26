69ad1d6325aaca47bf041afa8dc7f4148bafc576
amardkumar <102793539+amardkumar@users.noreply.github.com>
2025-05-26 13:14:29 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,6 @@
+//Time Complexity : push: O(1), pop: O(1), peek: O(1), is_empty: O(1), is_full: O(1)
+//Space Complexity : O(n), n is the number of items in the stack.
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,28 +11,49 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	return top < 0;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    	top =-1;
+    	
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    	
+    	if (top >= MAX - 1) { 
+            System.out.println("Stack Overflow"); 
+            return false; 
+        } 
+        a[++top] = x; 
+        return true; 
     } 
   
     int pop() 
     { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        //If empty return 0 and print " Stack Underflow" 
+    	//Write your code here
+    	
+        if (top < 0) { 
+            System.out.println("Stack Underflow"); 
+            return 0; 
+        } 
+        return a[top--]; 
     } 
   
     int peek() 
     { 
         //Write your code here
+        if (top < 0) { 
+            System.out.println("Stack is Empty"); 
+            return 0; 
+        } 
+        return a[top]; 
     } 
 } 
   
